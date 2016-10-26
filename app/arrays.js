12446f6f3c20c9c2dd91725d13695a67676d8381
Natalie Dodd <nataliedodd89@gmail.com>
2016-10-25 21:11:55 -0400

    first two questions complete

--- app/arrays.js
+++ app/arrays.js
@@ -2,11 +2,15 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+      index = arr.indexOf(item);
+      return index
+    },
 
-  },
 
   sum: function(arr) {
-
+    sum = arr.reduce( function(total, num) {
+      return total + num }, 0);
+      return sum
   },
 
   remove: function(arr, item) {
